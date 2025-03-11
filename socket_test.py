60aca678c077704b65c7168cdbb32cc0fe769472
Legotekton <legotekton@gmail.com>
2025-03-11 17:18:14 -0400

    created socket_test.py

---
+++ socket_test.py
@@ -0,0 +1,9 @@
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("10.42.0.1", 5005))
+
+print("Listening for messages...")
+while True:
+    data, addr = sock.recvfrom(1024)
+    print(f"Received message: {data.decode()} from {addr}")
