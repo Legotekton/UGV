43475159723d91d02cb81e093d9f07da5032d7e2
Legotekton <legotekton@gmail.com>
2025-03-11 19:25:09 -0400

    changes

--- socket_test.py
+++ socket_test.py
@@ -1,8 +1,12 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+UGV_IP = "10.42.0.120"  # Update with actual UGV IP
+PORT = 5005
 
-print("Listening for messages...")
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UGV_IP, PORT))
+
+print("Listening for messages from UAV...")
 while True:
     data, addr = sock.recvfrom(1024)
     print(f"Received message: {data.decode()} from {addr}")
