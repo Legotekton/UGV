e595a0770419834040648189be2ba70e42ce9660
Legotekton <legotekton@gmail.com>
2025-03-11 17:31:04 -0400

    CHANGED Port

--- socket_test.py
+++ socket_test.py
@@ -1,7 +1,7 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("10.42.0.1", 5005))
+sock.bind(("10.42.0.1", 9999))
 
 print("Listening for messages...")
 while True:
