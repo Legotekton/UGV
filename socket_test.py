a318aacd5772bb313bdf9377cbba3f4a5f050e43
Legotekton <legotekton@gmail.com>
2025-03-11 17:37:31 -0400

    changes

--- socket_test.py
+++ socket_test.py
@@ -1,7 +1,7 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("10.42.0.1", 9999))
+sock.bind(("10.42.0.1", 5005))
 
 print("Listening for messages...")
 while True:
