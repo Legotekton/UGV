3bd32dfacf2c293c1024716bc839248043711855
Legotekton <legotekton@gmail.com>
2025-03-19 18:10:41 -0400

    changes

--- goto_tests/UGV_telem_test.py
+++ goto_tests/UGV_telem_test.py
@@ -99,9 +99,9 @@ print("MAIN:  Code Started")
 vehicle = connectRover()
 print("Vehicle connected")
 
-manaul_arm()
-
 telem_link = setup_telem_connection()
+
+manaul_arm()
 
 while True:
     # Wait for the next GPS_RAW_INT message
