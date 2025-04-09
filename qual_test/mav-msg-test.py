0129e682fbc73496e109d05aee39c58cb0a89ae6
Legotekton <legotekton@gmail.com>
2025-04-09 17:52:34 -0400

    change

--- qual_test/mav-msg-test.py
+++ qual_test/mav-msg-test.py
@@ -83,7 +83,7 @@ def goto_waypoint(lat,lon, alt, waypoint_number):
         current_location = vehicle.location.global_relative_frame
         distance = distance_to(LocationGlobalRelative(lat, lon, alt), current_location)
 
-        if distance < 0.1:  # Stop when within 1 meter of the target
+        if distance < 0.25:  # Stop when within 1 meter of the target
             print(f"Reached waypoint {waypoint_number}")
             break
 
