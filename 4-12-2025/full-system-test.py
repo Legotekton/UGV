04f6b3406f4cefe45103e86e13624eaea2b2f49b
Legotekton <legotekton@gmail.com>
2025-04-17 17:31:26 -0400

    h

--- 4-12-2025/full-system-test.py
+++ 4-12-2025/full-system-test.py
@@ -74,6 +74,7 @@ def goto_waypoint(lat,lon, alt, waypoint_number):
     while True:
         while vehicle.velocity[0] != 0 or vehicle.velocity[1] != 0 or vehicle.velocity[2] != 0:
           time.sleep(0.5)
+        time.sleep(2)
         if vehicle.velocity[0] == 0 and vehicle.velocity[1] == 0 and vehicle.velocity[2] == 0:
              break
 
