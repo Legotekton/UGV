073ccba41c45c44657465a143adea3744ac55350
Legotekton <legotekton@gmail.com>
2025-04-20 18:09:14 -0400

    changes

--- 4-12-2025/full-system-test-no-wait.py
+++ 4-12-2025/full-system-test-no-wait.py
@@ -190,8 +190,8 @@ time.sleep(8)
 goto_waypoint(lat,lon,alt, 1)
 
 start_time = time.time()
-while time.time() - start_time < 0.15:
-  send_ned_velocity(1,0,0)
+while time.time() - start_time < 0.1:
+  send_ned_velocity(0.5,0,0)
 
 set_servo_pwm(4, 1000)
 time.sleep(7)
